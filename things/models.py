a925ad6ca8cc182b96d53f1060983cfc1b3f64aa
Murbarkh <murbarkh.hamid@gmail.com>
2023-10-03 19:11:14 +0100

    Fix error error due to lack of parameter

--- things/models.py
+++ things/models.py
@@ -5,6 +5,6 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=30)
     description = models.TextField()
     quanitity = models.IntegerField()

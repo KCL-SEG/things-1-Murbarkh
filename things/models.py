e3dd34a823cc8ab9b0657204c850a6ff4337255d
Murbarkh <murbarkh.hamid@gmail.com>
2023-10-03 19:20:35 +0100

    Refine the Thing model to have constraints

--- things/models.py
+++ things/models.py
@@ -5,6 +5,15 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.TextField()
-    quanitity = models.IntegerField()
+    name = models.CharField(
+        max_length=30,
+        unique=True,
+        blank=False
+        )
+    description = models.CharField(
+        max_length=120,
+    )
+    quanitity = models.IntegerField(
+        max_value= 100,
+        min_value = 0
+    )

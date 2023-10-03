e23892cbbb421a65dd2f7b6f50362a1582e338d9
Murbarkh <murbarkh.hamid@gmail.com>
2023-10-03 19:56:21 +0100

    Test Model

--- things/models.py
+++ things/models.py
@@ -6,4 +6,6 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    pass
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
+    quantity = models.PositiveIntegerField()

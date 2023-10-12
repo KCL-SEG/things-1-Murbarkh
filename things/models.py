8002cb8826c6a2aa88909e19661583a3a119734f
Murbarkh <murbarkh.hamid@gmail.com>
2023-10-12 11:30:22 +0100

    Refine Things Model

--- things/models.py
+++ things/models.py
@@ -1,11 +1,20 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.exceptions import ValidationError
 
 
 
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=120)
-    quantity = models.PositiveIntegerField()
+    name = models.CharField(
+        max_length=30,
+        unique=True,
+        blank=False
+                            )
+    description = models.CharField(
+        max_length=120,
+        unique=False,
+        blank=True,
+                                   )
+    quantity = models.PositiveIntegerField(validators=[MaxValueValidator(100)])

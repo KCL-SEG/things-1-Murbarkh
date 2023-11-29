291966465d7956d27044d5b7e695f82a1e40034a
Murbarkh <murbarkh.hamid@gmail.com>
2023-11-29 20:22:34 +0000

    Changed back to models

--- things/models.py
+++ things/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.core.exceptions import ValidationError
-from django.contrib.auth.models import AbstractUser
+
 
 
 
 
 # Create your models here.
-class Thing(AbstractUser):
+class Thing(models.Model):
     name = models.CharField(
         max_length=30,
         unique=True,
